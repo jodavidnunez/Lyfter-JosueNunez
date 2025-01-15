bc9f00c07d824b377a0818abb93f47abeb6868d7
Josue Nuñez <jodavidnunez22@gmail.com>
2025-01-14 20:51:39 -0600

    removing excercises from previous commit

--- semana_4/Semana_4_Ejercicio_3_Python.py
+++
@@ -1,9 +0,0 @@
-# 3. Cree un programa con un numero secreto del 1 al 10. 
-# El programa no debe cerrarse hasta que el usuario adivine el numero.
-
-import random
-numero_aleatorio = random.randint(1,10)
-numero_usuario = int(input("Cual es el numero?: "))
-while numero_usuario != numero_aleatorio:
-    numero_usuario = int(input("Lo siento, no adivinaste! Por favor ingrese un nuevo numero: "))
-print(f'Felicidades adivinaste! El numero secreto es {numero_aleatorio}')
--- semana_4/Semana_4_Ejercicio_4_Python.py
+++
@@ -1,8 +0,0 @@
-# 4. Cree un programa que le pida tres números al usuario y muestre el mayor.
-list_numeros_a_evaluar = []
-list_numeros_a_evaluar.append(int(input("Por favor ingrese el primer numero: ")))
-list_numeros_a_evaluar.append(int(input("Por favor ingrese el segundo numero: ")))
-list_numeros_a_evaluar.append(int(input("Por favor ingrese el tercer numero: ")))
-list_numeros_ordenados = sorted(list_numeros_a_evaluar, reverse=True)
-numero_mayor = list_numeros_ordenados[0]
-print(f'El numero mayor es: {numero_mayor}')
