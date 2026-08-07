f141c59fd40ed246b33acf7080c8948ae09076cb
Josue Nuñez <jodavidnunez22@gmail.com>
2026-08-07 08:25:59 -0600

    Ejercicio 1 de bases de datos - correcion de FK - debe ir en invoices en vez de en shopping cart

--- M3/semana_1/semana1_bases_de_datos_ejercicio1.sql
+++ M3/semana_1/semana1_bases_de_datos_ejercicio1.sql
@@ -10,9 +10,10 @@ CREATE TABLE `Products`(
 CREATE TABLE `Invoices`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `Invoice_number` INT NOT NULL,
-    `Purchace_date` DATE NOT NULL,
+    `Purchase_date` DATE NOT NULL,
     `Buyer_email` VARCHAR(255) NOT NULL,
-    `Total_amount` DECIMAL(8, 2) NOT NULL
+    `Total_amount` DECIMAL(8, 2) NOT NULL,
+    `FK_Shopping_Cart` INT NOT NULL
 );
 CREATE TABLE `Products_per_invoice (Junction Table)`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -21,24 +22,23 @@ CREATE TABLE `Products_per_invoice (Junction Table)`(
     `Quantity` INT NOT NULL,
     `Total_amount` DECIMAL(8, 2) NOT NULL
 );
-CREATE TABLE `Shopping_car`(
+CREATE TABLE `Shopping_cart`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Buyer_email` VARCHAR(255) NOT NULL,
-    `FK_invoices` INT NOT NULL
+    `Buyer_email` VARCHAR(255) NOT NULL
 );
 CREATE TABLE `Products_per_shopping_car (Junction Table)`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `FK_Shopping_car` INT NOT NULL,
+    `FK_Shopping_Cart` INT NOT NULL,
     `FK_Product` INT NOT NULL,
     `Quantity` INT NOT NULL,
     `Total_amount` DECIMAL(8, 2) NOT NULL
 );
 ALTER TABLE
-    `Products_per_shopping_car (Junction Table)` ADD CONSTRAINT `products_per_shopping_car (junction table)_fk_shopping_car_foreign` FOREIGN KEY(`FK_Shopping_car`) REFERENCES `Shopping_car`(`id`);
+    `Products_per_shopping_car (Junction Table)` ADD CONSTRAINT `products_per_shopping_car (junction table)_fk_shopping_cart_foreign` FOREIGN KEY(`FK_Shopping_Cart`) REFERENCES `Shopping_cart`(`id`);
+ALTER TABLE
+    `Invoices` ADD CONSTRAINT `invoices_fk_shopping_cart_foreign` FOREIGN KEY(`FK_Shopping_Cart`) REFERENCES `Shopping_cart`(`id`);
 ALTER TABLE
     `Products_per_shopping_car (Junction Table)` ADD CONSTRAINT `products_per_shopping_car (junction table)_fk_product_foreign` FOREIGN KEY(`FK_Product`) REFERENCES `Products`(`id`);
-ALTER TABLE
-    `Shopping_car` ADD CONSTRAINT `shopping_car_fk_invoices_foreign` FOREIGN KEY(`FK_invoices`) REFERENCES `Products`(`id`);
 ALTER TABLE
     `Products_per_invoice (Junction Table)` ADD CONSTRAINT `products_per_invoice (junction table)_fk_invoices_foreign` FOREIGN KEY(`FK_Invoices`) REFERENCES `Invoices`(`id`);
 ALTER TABLE
